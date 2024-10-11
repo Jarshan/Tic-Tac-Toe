6a074110f747831f6dd62ba569dc6da584897029
Jarshan <jarshanjauffar@gmail.com>
2024-10-11 11:14:24 +0530

    Adding styles

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -8,6 +8,12 @@ export const TicTacToe = () => {
     <div className='container'>
         <h1 className="title">Tic Tac Toe Game In <span>React</span></h1>
         <div className="board">
+            <div className='row1'>
+                <div className='boxes'></div>
+                <div className='boxes'></div>
+                <div className='boxes'></div>
+            </div>
+
 
         </div>
         <button className="reset">Reset</button>
