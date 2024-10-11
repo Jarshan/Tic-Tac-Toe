de987de8552d486461da90a624c1ca0ac87d3b07
Jarshan <jarshanjauffar@gmail.com>
2024-10-11 11:46:02 +0530

    Adding circle & cross

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -10,8 +10,7 @@ export const TicTacToe = () => {
     let [count,setCount] = useState(0);
     let [lock,setLock] = useState(false);
 
-
-    const toggle = () => {
+    const toggle = (e,num) => {
         if (lock) {
             return 0;
         }
@@ -38,21 +37,21 @@ export const TicTacToe = () => {
         <div className="board">
 
             <div className='row1'>
-                <div className='boxes'></div>
-                <div className='boxes'></div>
-                <div className='boxes'></div>
+                <div className='boxes' onClick={(e)=>{toggle(e,0)}}></div>
+                <div className='boxes' onClick={(e)=>{toggle(e,1)}}></div>
+                <div className='boxes' onClick={(e)=>{toggle(e,2)}}></div>
             </div>
 
             <div className='row2'>
-                <div className='boxes'></div>
-                <div className='boxes'></div>
-                <div className='boxes'></div>
+                <div className='boxes' onClick={(e)=>{toggle(e,3)}}></div>
+                <div className='boxes' onClick={(e)=>{toggle(e,4)}}></div>
+                <div className='boxes' onClick={(e)=>{toggle(e,5)}}></div>
             </div>
 
             <div className='row3'>
-                <div className='boxes'></div>
-                <div className='boxes'></div>
-                <div className='boxes'></div>
+                <div className='boxes' onClick={(e)=>{toggle(e,6)}}></div>
+                <div className='boxes' onClick={(e)=>{toggle(e,7)}}></div>
+                <div className='boxes' onClick={(e)=>{toggle(e,8)}}></div>
             </div>
 
 
